b4d37cf41b1479b2b5b7cc18a15314ba18474a93
fadyyousry <fadyyousry20182gmail.com>
2017-12-01 20:25:13 +0200

    file corrupted

--- client.cpp
+++ client.cpp
@@ -17,7 +17,7 @@
 #include <sys/time.h>
 
 #define ECHOMAX 500
-#define MAX_PACKET_SIZE 512
+#define MAX_PACKET_SIZE 500
 using namespace std;
 
 struct ack_packet
@@ -41,7 +41,7 @@ struct packet
 // Global Variables
 unsigned short server_port;
 unsigned short client_port;
-char *serv_IP = (char*)"127.0.0.1";
+char *serv_IP;;
 char *file_name;
 int sock; /* Socket descriptor */
 struct sockaddr_in server_address; /* Echo server address */
@@ -52,12 +52,14 @@ int window_size;
 // Functions Prototypes
 void read_client_file();
 void handle_stope_and_wait();
+void add_packet_to_file(char [], int );
+void handle_selective_repeat();
 // Main
 int main(int argc, char *argv[])
 {
     read_client_file();
     char *sent_string;
-    char received_buffer[MAX_PACKET_SIZE+1];
+    char received_buffer[MAX_PACKET_SIZE + 1];
     int sent_stringLen;
     int respStringLen;
     /* Create a datagram/UDP socket */
@@ -67,67 +69,134 @@ int main(int argc, char *argv[])
     memset(&server_address, 0, sizeof(server_address)); /* Zero out structure */
     server_address.sin_family = AF_INET; /* Internet addr family */
     server_address.sin_addr.s_addr = inet_addr(serv_IP); /* Server IP address */
-   // cout << "HI1 " << server_address.sin_addr.s_addr<<endl;
+    // cout << "HI1 " << server_address.sin_addr.s_addr<<endl;
     server_address.sin_port = htons(server_port); /* Server port */
 
-
+    cout << "Server IP: \n" << serv_IP;
+    cout << "Server port : \n" <<server_port;
+    cout << "Socket Created\n";
     //***********************************************Socket Created ***********************************************************//
 
     // SENDING FILE NAME:
     struct packet file_name_pckt;
     file_name_pckt.len = strlen(file_name);
     memcpy(file_name_pckt.data, file_name, (strlen(file_name) + 1) * sizeof *file_name);
-
+    cout << "Data: " << file_name_pckt.data << endl;
     if (sendto(sock, &file_name_pckt, sizeof(file_name_pckt), 0, (struct sockaddr *) &server_address, sizeof(server_address)) != sizeof(file_name_pckt))
+    {
         printf("sendto() sent a different number of bytes than expected\n");
+    }
+    else
+    {
+        cout << "Client Sent File Name" << endl;
+    }
 
-     // Receiving Acknowledgement
-    /* Recv a response */
+
+
+
+
+
+
+
+/*
+    // Receiving Acknowledgement
+    // Recv a response
     struct ack_packet *file_name_ack;
     fromSize = sizeof(from_add) ;
     if ((respStringLen = recvfrom(sock, received_buffer, MAX_PACKET_SIZE, 0, (struct sockaddr *) &from_add, &fromSize)) < 0 )
+    {
         printf("recvfrom() failed\n") ;
+    }
+    else
+    {
+        cout << "Client Received File Name Acknowledgement" << endl;
+    }
+
     if (server_address.sin_addr.s_addr != from_add.sin_addr.s_addr)
     {
         fprintf(stderr,"Error: received a packet from unknown source.\n %d\n%d\n", server_address.sin_addr.s_addr, from_add.sin_addr.s_addr);
         exit(1);
     }
-    /* null-terminate the received data */
+    // null-terminate the received data
     received_buffer[respStringLen] = '\0' ;
-    printf("Received: %s\n", received_buffer); /* Print the  echoed arg */
+    printf("Received: %s\n", received_buffer); // Print the  echoed arg
     file_name_ack = (ack_packet*) received_buffer;
-    // Convert received to ack_packet here
+
+    // Convert received to ack_packet
+
+
+    */
 
     handle_stope_and_wait();
-    // Receiving the file packets
     close(sock);
     exit(0);
 }
 
 void handle_stope_and_wait()
 {
-    int last_seq_no = 1;
+    int expected_seq_no = 0;
     struct packet *file_pckt;
     struct ack_packet ack;
     char received_buffer[MAX_PACKET_SIZE + 1];
     int recievedLength;
     while (true)
     {
-    if ((recievedLength = recvfrom(sock, received_buffer, MAX_PACKET_SIZE, 0, (struct sockaddr *) &from_add, &fromSize)) < 0 )
-        printf("recvfrom() failed\n") ;
-    file_pckt = (packet*) received_buffer;
-    cout << "Recieved a file packet\n";
+        cout << "Waiting for a file packet..." << endl;
+        if ((recievedLength = recvfrom(sock, received_buffer, MAX_PACKET_SIZE, 0, (struct sockaddr *) &from_add, &fromSize)) < 0 ) {
+            printf("recvfrom() failed\n");
+        } else {
+            cout << "Received a File Packet" << endl;
+            received_buffer[recievedLength] = '\0';
+            file_pckt = (packet*) received_buffer;
+            add_packet_to_file(file_pckt->data, file_pckt->len);
+            if (file_pckt->seqno == expected_seq_no)
+            {
+                cout << "expected_seq_no" ;
+                ack.ackno = expected_seq_no;
+                expected_seq_no = !expected_seq_no;
 
-    if (sendto(sock, &ack, sizeof(ack), 0, (struct sockaddr *) &server_address, sizeof(server_address)) != sizeof(ack))
-        printf("Failed to send ack \n");
+                if (sendto(sock, &ack, sizeof(ack), 0, (struct sockaddr *) &server_address, sizeof(server_address)) != sizeof(ack))
+                    printf("Failed to send ack \n");
+                else
+                    cout << "Sent Ack" << endl;
+            }
+            else {
+                cout << "field to send ack" << endl;
+            }
+
+        }
     }
 
+}
+
+void handle_selective_repeat()
+{
+
+
+}
+
+void add_packet_to_file(char data[], int data_size)
+{
+    ofstream myfile;
+    myfile.open(file_name);
+    char *temp = NULL;
+    temp = (char*)malloc((data_size+1) * sizeof *temp);
+    memcpy(temp,(char*)data, data_size);
+    myfile.write((char *)temp, data_size);
+    free(temp);
 }
 
 void read_client_file()
 {
     ifstream file("client.in");
     string str;
+
+    // getting server IP
+    getline(file, str);
+    serv_IP = (char*)malloc(str.length() * sizeof *serv_IP);
+    memcpy(serv_IP, str.c_str(), str.length() + 1);
+
+
     // getting server_port
     getline(file, str);
     server_port = atoi(str.c_str());
@@ -141,7 +210,7 @@ void read_client_file()
     file_name = (char*)malloc(str.length() * sizeof *file_name);
     memcpy(file_name, str.c_str(), str.length() + 1);
 
-    // getting client_port
+    // getting window Size
     getline(file, str);
     window_size = atoi(str.c_str());
 
--- server.cpp
+++ server.cpp
@@ -16,8 +16,7 @@
 #include <bits/stdc++.h>
 #include <sys/ioctl.h>
 
-#define ECHOMAX 255
-#define MAX_PACKET_LENGTH 512
+#define MAX_PACKET_LENGTH 500
 
 using namespace std;
 
@@ -27,7 +26,7 @@ struct packet {
 	uint16_t len;
 	uint32_t seqno;
 	/*data*/
-	char data[MAX_PACKET_LENGTH];
+	char data[MAX_PACKET_LENGTH - 8];
 };
 
 struct ack_packet {
@@ -57,25 +56,29 @@ bool dropPacket(unsigned int seed, double probability)
 	return ((double) rand() / (RAND_MAX)) < probability ; 
 }
 
-vector<char> readFile(string fileName)
+vector<char> readFile(string filename)
 {
-    ifstream ifs(fileName, ios::binary|ios::ate);
+    ifstream ifs(filename, ios::binary|ios::ate);
+
     ifstream::pos_type pos = ifs.tellg();
-    vector<char>  res(pos);
+
+    vector<char>  result(pos);
 
     ifs.seekg(0, ios::beg);
-    ifs.read(&res[0], pos);
-    return res;
+    ifs.read(&result[0], pos);
+    return result;
 }
 
 vector<packet> getFilePackets(string fileName)
 {
 	vector<packet> packets ;
+	cout << "begin read file \n" ;
 	vector<char> file_data = readFile(fileName) ;
+	cout << "file readed\n" ;
 	int index = 0 ;
 	uint32_t i = 0  ;
 	vector<char>::iterator it = file_data.begin();
-
+	cout << "begin iterate\n" ;
 	for (; i < file_data.size()/MAX_PACKET_LENGTH + 1; i++)
 	{
 		uint16_t len;
@@ -94,14 +97,14 @@ vector<packet> getFilePackets(string fileName)
 		}
 
 		// assigning data
-		char data[MAX_PACKET_LENGTH];
+		char data[MAX_PACKET_LENGTH - 8];
 		//copy(it, it+len, data)  ;
 
 		// assinging data and header to packet to be send
 		packet p  ;
 		p.cksum = 0 ;  // checksum
 		p.len = len ;  // length
-		p.seqno = i ; // sequence number
+		p.seqno = i%2 ; // sequence number
 		copy(it, it+len, p.data) ; // data
 
 		packets.push_back(p) ;
@@ -123,14 +126,14 @@ int main(int argc, char *argv[])
 	struct sockaddr_in echoServAddr;
 	struct sockaddr_in echoClntAddr;
 	unsigned int cliAddrLen;
-	struct packet echoBuffer;
+	//struct packet echoBuffer;
 	unsigned short echoServPort;
 	int recvMsgSize;
 
 	int max_window, seed;
     double p;
 	echoServPort = getArguments(max_window, seed, p);
-
+	cout << echoServPort << endl;
 
 	if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
 		printf("socket() failed");
@@ -138,36 +141,57 @@ int main(int argc, char *argv[])
 	memset(&echoServAddr, 0, sizeof(echoServAddr));
 	echoServAddr.sin_family = AF_INET;
 	echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	echoServAddr.sin_port = echoServPort;
+	echoServAddr.sin_port = htons(echoServPort);
 
 	if (bind(sock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
 		printf("bind() failed");
 
-
-	for (;;) /* Run forever */
-	{
+	cout << "bind" << endl;
+	//cout << echoServAddr.sin_addr.s_addr << endl;
+	//for (int i=0;i < 1; i++) /* Run forever */
+	while(true){
+		char packetBuffer[MAX_PACKET_LENGTH] ;
 		cliAddrLen = sizeof(echoClntAddr);
-		if ((recvMsgSize = recvfrom(sock, &echoBuffer, MAX_PACKET_LENGTH, 0, (struct sockaddr *) &echoClntAddr, &cliAddrLen)) < 0)
-			printf("recvfrom() failed") ;
-		string filename = getfilename(echoBuffer);
+		if ((recvMsgSize = recvfrom(sock, packetBuffer, MAX_PACKET_LENGTH, 0, (struct sockaddr *) &echoClntAddr, &cliAddrLen)) < 0)
+			printf("recvfrom() failed\n");
 		printf("Handling client %s\n", inet_ntoa(echoClntAddr.sin_addr)) ;
-		if (!fork())
+		cout <<  "recvMsgSizes "<< recvMsgSize << endl;
+		struct packet * file_pckt = (packet*) packetBuffer;
+		string filename = getfilename(*file_pckt);
+		cout << "file name : " << filename<< endl;
+		int proccess  = fork();
+		cout << "proccess number" << proccess <<endl;
+		if (!proccess)
 		{
+			cout << "getFilePackets B" << endl;
 			vector<packet> packets = getFilePackets(filename);
+			cout << "getFilePackets A" << endl;
 			int index = 0;
+			cout << "size :" << packets.size() << endl;
 			while (index < packets.size()) {
-				if (sendto(sock, &packet[index], recvMsgSize, 0, (struct sockaddr *) &echoClntAddr, sizeof(echoClntAddr)) < 0)
+				cout << "seqno" << packets[index].seqno << endl;
+				cout << "size packet" << packets[index].len << endl;
+				if (sendto(sock, &packets[index], recvMsgSize, 0, (struct sockaddr *) &echoClntAddr, sizeof(echoClntAddr)) < 0)
 				{
 					printf("sendto() sent a different number of bytes than expected");
 				}
 				else {
 					// while not timeout
-					while ((recvMsgSize = recvfrom(sock, &echoBuffer, ECHOMAX, 0, (struct sockaddr *) &echoClntAddr, &cliAddrLen)) == 0) //not time out
+					recvMsgSize = 0;
+					while (recvMsgSize == 0) //not time out 
+					{
+						recvMsgSize = recvfrom(sock, packetBuffer, MAX_PACKET_LENGTH, 0, (struct sockaddr *) &echoClntAddr, &cliAddrLen);
+						cout << recvMsgSize << endl;
 						printf("increment time out");
+					}
 					if (recvMsgSize > 0)
 						index++;
+
 				}
+				cout << "index = " << index<< endl; 
 			}
+			cout << "exit" << endl;
+			exit(0);
 		}
 	}
 }
